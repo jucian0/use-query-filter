4b0cd4df0497b9cd818465459948f1d5be0af0ce
Jucian0 <juciano@outlook.com.br>
2021-08-08 11:06:46 -0300

    feat(usequeryfilter): improve arguments validation

--- src/hooks/useQueryFilter.ts
+++ src/hooks/useQueryFilter.ts
@@ -2,111 +2,105 @@ import { useState, useLayoutEffect } from 'react'
 import { FilterContextType } from './types'
 
 function removeEmptyProperties<T>(object: T) {
-   return Object.keys(object).reduce<T>((acc, ac) => {
-      if (
-         typeof (object as any)[ac] !== 'boolean' &&
-         (!(object as any)[ac] || (object as any)[ac] === '')
-      ) {
-         return acc
-      }
-      return {
-         ...acc,
-         [ac]: (object as any)[ac],
-      }
-   }, Object.assign({}))
+  return Object.keys(object).reduce<T>((acc, ac) => {
+    if (
+      typeof (object as any)[ac] !== 'boolean' &&
+      (!(object as any)[ac] || (object as any)[ac] === '')
+    ) {
+      return acc
+    }
+    return {
+      ...acc,
+      [ac]: (object as any)[ac],
+    }
+  }, Object.assign({}))
 }
 
-export function useQueryFilter<
-   TFilter extends { page?: number; limit?: number }
->(
-   initialState: TFilter,
-   callbackOnInit?: (state: TFilter) => void,
+export function useQueryFilter<TFilter extends Object>(
+  initialState: TFilter,
+  callbackOnInit?: (state: TFilter) => void
 ): FilterContextType<TFilter> {
-   const [filter, setFilter] = useState<TFilter>(
-      parseObjectFilter(new URLSearchParams(window.location.search)),
-   )
+  if (typeof initialState !== 'object' || Array.isArray(initialState)) {
+    throw Error('`initialState` argument should be an object.')
+  }
 
-   function parseObjectFilter(otherParams: any) {
-      return Array.from(otherParams.keys()).reduce<TFilter>(
-         (acc, ac, index) => {
-            const value = Array.from(otherParams.values())[index] as string
+  if (typeof callbackOnInit !== 'function') {
+    throw Error('`callbackOnInit` argument should be a function.')
+  }
+  const [filter, setFilter] = useState<TFilter>(
+    parseObjectFilter(new URLSearchParams(window.location.search))
+  )
 
-            if (!isNaN(parseInt(value)) && !value.includes('-')) {
-               return {
-                  ...acc,
-                  [ac as string]: parseInt(value),
-               }
-            }
+  function parseObjectFilter(otherParams: any) {
+    return Array.from(otherParams.keys()).reduce<TFilter>((acc, ac, index) => {
+      const value = Array.from(otherParams.values())[index] as string
 
-            return {
-               ...acc,
-               [ac as string]: value,
-            }
-         },
-         Object.assign({}),
-      )
-   }
-
-   function set(tvalue: Partial<TFilter> | ((filter: TFilter) => TFilter)) {
-      const value = typeof tvalue === 'function' ? tvalue(filter) : tvalue
-
-      const filteredEmpty = removeEmptyProperties({ ...filter, ...value })
-
-      const params = new URLSearchParams(filteredEmpty as any)
-
-      setFilter((state) => ({ ...state, ...value }))
-
-      window.history.replaceState(
-         {},
-         '',
-         `${window.location.pathname}?${params}`,
-      )
-
-      return new Promise<TFilter>((resolve) => {
-         setTimeout(() => {
-            resolve({ ...filter, ...value })
-         }, 500)
-      })
-   }
-
-   async function reset(fn?: (e: TFilter) => void) {
-      setFilter(initialState)
-
-      const params = new URLSearchParams(initialState as {})
-
-      window.history.replaceState(
-         {},
-         '',
-         `${window.location.pathname}?${params}`,
-      )
-
-      return new Promise<TFilter>((resolve) => {
-         setTimeout(() => {
-            typeof fn === 'function' ? fn(initialState) : null
-            resolve(initialState)
-         }, 500)
-      })
-   }
-
-   useLayoutEffect(() => {
-      const urlSearch = window.location.search
-      const params = parseObjectFilter(new URLSearchParams(urlSearch))
-      if (!!urlSearch) {
-         setFilter(params)
-         callbackOnInit?.(params)
-      } else {
-         set({ ...params, ...initialState })
-         callbackOnInit?.({ ...params, ...initialState })
+      if (!isNaN(parseInt(value)) && !value.includes('-')) {
+        return {
+          ...acc,
+          [ac as string]: parseInt(value),
+        }
       }
-      //eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [])
 
-   return [
-      filter,
-      {
-         set,
-         reset,
-         query:window.location.search
-      },
-   ]
+      return {
+        ...acc,
+        [ac as string]: value,
+      }
+    }, Object.assign({}))
+  }
+
+  function set(tvalue: Partial<TFilter> | ((filter: TFilter) => TFilter)) {
+    const value = typeof tvalue === 'function' ? tvalue(filter) : tvalue
+
+    const filteredEmpty = removeEmptyProperties({ ...filter, ...value })
+
+    const params = new URLSearchParams(filteredEmpty as any)
+
+    setFilter((state) => ({ ...state, ...value }))
+
+    window.history.replaceState({}, '', `${window.location.pathname}?${params}`)
+
+    return new Promise<TFilter>((resolve) => {
+      setTimeout(() => {
+        resolve({ ...filter, ...value })
+      }, 500)
+    })
+  }
+
+  async function reset(fn?: (e: TFilter) => void) {
+    setFilter(initialState)
+
+    const params = new URLSearchParams(initialState as {})
+
+    window.history.replaceState({}, '', `${window.location.pathname}?${params}`)
+
+    return new Promise<TFilter>((resolve) => {
+      setTimeout(() => {
+        typeof fn === 'function' ? fn(initialState) : null
+        resolve(initialState)
+      }, 500)
+    })
+  }
+
+  useLayoutEffect(() => {
+    const urlSearch = window.location.search
+    const params = parseObjectFilter(new URLSearchParams(urlSearch))
+    if (!!urlSearch) {
+      setFilter(params)
+      callbackOnInit?.(params)
+    } else {
+      set({ ...params, ...initialState })
+      callbackOnInit?.({ ...params, ...initialState })
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return [
+    filter,
+    {
+      set,
+      reset,
+      query: window.location.search,
+    },
+  ]
 }
